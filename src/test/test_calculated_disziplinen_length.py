2fa95620cf98215c9e5e1da2984aa7f7785a69df
Andi Meier <andi@daemeier-clan.ch>
2023-10-08 16:18:01 +0200

    test: add tests for disziplinen length calculation

---
+++ src/test/test_calculated_disziplinen_length.py
@@ -0,0 +1,36 @@
+import unittest
+
+import athletics_event
+
+
+class TestCalculatedDisziplinenLength(unittest.TestCase):
+    def test_u12w_60m(self):
+        from .u12w_4k_event_data import event_data as u12w_4k_event_data
+        athletics_event.interactive_main(u12w_4k_event_data, ["saturday", "--print-scenario-and-exit"])
+        self.assertEqual(1, athletics_event.event._disziplinen["U12W_4K_Gr14_60m"].length)
+        self.assertEqual(1, athletics_event.event._disziplinen["U12W_4K_Gr15_to_Gr16_60m"].length)
+        self.assertEqual(1, athletics_event.event._disziplinen["U12W_4K_Gr17_60m"].length)
+        self.assertEqual(2, athletics_event.event._disziplinen["U12W_4K_Gr18_60m"].length)
+
+    def test_u12w_kugel(self):
+        from .u12w_4k_event_data import event_data as u12w_4k_event_data
+        athletics_event.interactive_main(u12w_4k_event_data, ["saturday", "--print-scenario-and-exit"])
+        self.assertEqual(3, athletics_event.event._disziplinen["U12W_4K_Gr14_Kugel"].length)
+        self.assertEqual(3, athletics_event.event._disziplinen["U12W_4K_Gr15_Kugel"].length)
+        self.assertEqual(3, athletics_event.event._disziplinen["U12W_4K_Gr16_Kugel"].length)
+        self.assertEqual(3, athletics_event.event._disziplinen["U12W_4K_Gr17_Kugel"].length)
+        self.assertEqual(3, athletics_event.event._disziplinen["U12W_4K_Gr18_Kugel"].length)
+
+    def test_u12w_weit(self):
+        from .u12w_4k_event_data import event_data as u12w_4k_event_data
+        athletics_event.interactive_main(u12w_4k_event_data, ["saturday", "--print-scenario-and-exit"])
+        self.assertEqual(4, athletics_event.event._disziplinen["U12W_4K_Gr14_Weit"].length)
+        self.assertEqual(4, athletics_event.event._disziplinen["U12W_4K_Gr15_Weit"].length)
+        self.assertEqual(4, athletics_event.event._disziplinen["U12W_4K_Gr16_Weit"].length)
+        self.assertEqual(4, athletics_event.event._disziplinen["U12W_4K_Gr17_Weit"].length)
+        self.assertEqual(4, athletics_event.event._disziplinen["U12W_4K_Gr18_Weit"].length)
+
+    def test_u12w_600m(self):
+        from .u12w_4k_event_data import event_data as u12w_4k_event_data
+        athletics_event.interactive_main(u12w_4k_event_data, ["saturday", "--print-scenario-and-exit"])
+        self.assertEqual(6, athletics_event.event._disziplinen["U12W_4K_Gr14_to_Gr18_600m"].length)
