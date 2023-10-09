806effaf6d9bf191177e6e675687f1a56cc56001
Andi Meier <andi@daemeier-clan.ch>
2023-10-09 13:13:30 +0200

    test: accelerate tests

--- src/test/test_athletics_event.py
+++ src/test/test_athletics_event.py
@@ -9,17 +9,17 @@ class TestAthleticsEvent(unittest.TestCase):
 
     def test_interactive_main_u12w_4k_event(self):
         from .u12w_4k_event_data import event_data as u12w_4k_event_data
-        athletics_event.interactive_main(u12w_4k_event_data, ["saturday"])
-        self.assertEqual(300, athletics_event.event.scenario.objective_value())
+        athletics_event.interactive_main(u12w_4k_event_data, ["saturday", "--ratio-gap=1"])
+        self.assertEqual(291, athletics_event.event.scenario.objective_value())
 
     def test_interactive_main_man_6k_event(self):
         from .man_6k_event_data import event_data as man_6k_event_data
-        athletics_event.interactive_main(man_6k_event_data, ["sunday"])
-        self.assertEqual(419, athletics_event.event.scenario.objective_value())
+        athletics_event.interactive_main(man_6k_event_data, ["sunday", "--ratio-gap=1"])
+        self.assertEqual(426, athletics_event.event.scenario.objective_value())
 
     def test_interactive_main_man_10k_event(self):
         from .man_10k_event_data import event_data as man_10k_event_data
-        athletics_event.interactive_main(man_10k_event_data, ["saturday"])
+        athletics_event.interactive_main(man_10k_event_data, ["saturday", "--ratio-gap=1"])
         self.assertEqual(397, athletics_event.event.scenario.objective_value())
-        athletics_event.interactive_main(man_10k_event_data, ["sunday"])
+        athletics_event.interactive_main(man_10k_event_data, ["sunday", "--ratio-gap=1"])
         self.assertEqual(589, athletics_event.event.scenario.objective_value())
